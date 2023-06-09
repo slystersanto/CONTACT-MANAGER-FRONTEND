fb0403f13839bbcea11bd9a09ca483d288666e07
slystersanto <santhosh.mech.19@gmail.com>
2023-06-09 14:18:04 +0530

    sas

--- src/Compnents/Navbar.js
+++ src/Compnents/Navbar.js
@@ -1,40 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 const Navbar = () => {
   return (
-    <nav className='navbar navbar-dark bg-dark navbar-expand-sm'>
+    <React.Fragment>
+     <nav className='navbar navbar-dark bg-dark navar-expand-sm'>
       <div className='container'>
         <Link to='/contactlist' className='navbar-brand'>
-          <i className='bx bx-mobile text-warning' /> Contact <span className='text-warning'>Manager</span>
-        </Link>
-        <div className='collapse navbar-collapse' id='navbarNav'>
-          <ul className='navbar-nav'>
-            <li className='nav-item'>
-              <Link to='/contactlist' className='nav-link'>
-                Contacts
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link to='/addcontact' className='nav-link'>
-                Add Contact
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link to='/login' className='nav-link'>
-                Login
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link to='/register' className='nav-link'>
-                Register
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
-};
+        <i className='bx bx-mobile text-warning'/>Contact <span className='text-warning'> Manager</span></Link>
 
-export default Navbar;
+      </div>
+
+     </nav>
+        
+    </React.Fragment>
+  )
+}
+
+export default Navbar
